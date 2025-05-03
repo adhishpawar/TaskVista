3685d86570aada3791d9df96e4193f097db37c70
adhish <adhishpawar@gmail.com>
2025-05-03 10:52:50 +0530

    prj setup

---
+++ src/main/java/com/taskvista/taskvista/TaskVistaApplication.java
@@ -0,0 +1,13 @@
+package com.taskvista.taskvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskVistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskVistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskvista/taskvista/TaskVistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskvista.taskvista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskVistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
